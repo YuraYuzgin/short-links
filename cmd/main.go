0d8287222251852f4c443b89cdc34b688c4084cb
Yuriy <yura.yuzgin@yandex.ru>
2025-06-17 00:45:49 +0300

    feat: add http server, database connection, crud, validation.

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"go/adv-demo/configs"
+	"go/adv-demo/internal/auth"
+	"go/adv-demo/internal/link"
+	"go/adv-demo/pkg/db"
+	"net/http"
+)
+
+func main() {
+	conf := configs.LoadConfig()
+	db := db.NewDb(conf)
+	router := http.NewServeMux()
+
+	// Repositories
+	linkRepository := link.NewLinkRepository(db)
+
+	// Handlers
+	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
+		Config: conf,
+	})
+	link.NewLinkHandler(router, link.LinkHandlerDeps{
+		LinkRepository: linkRepository,
+	})
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening on port 8081")
+	server.ListenAndServe()
+}
